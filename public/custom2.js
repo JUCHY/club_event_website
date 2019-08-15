aec8ab477e4d633f846dab57d07305d4e2137466
Joshua Tagoe <34180299+JUCHY@users.noreply.github.com>
2019-08-15 01:31:29 -0400

    improved picture preview for Events, and added API call to Gallery

--- public/custom2.js
+++ public/custom2.js
@@ -6,7 +6,7 @@ $(document).ready(function(){ userFeed = new Instafeed({
     resolution: 'standard_resolution',
     accessToken: '6059179875.1677ed0.dac65dca2f1c4ca797b70eeb7a32a560',
     sortBy: 'most-recent',
-    template: '<div class="gallery card"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" align="left"/></a><div class="content"><p>{{caption}}</p></div></div>',
+    template: '<div><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" align="left"/></a></div>',
 })
 userFeed.run();
 });
--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -1,10 +1,22 @@
 import React, {Component } from 'react';
+import './Gallery.css'
 
 export default class Gallery extends Component{
 
-    render(){
-        return (<div>
+    componentDidMount () {
+        const script = document.createElement("script");
+    
+        script.src = "custom2.js";
+        script.async = true;
+    
+        document.body.appendChild(script);
+    }
 
-        </div>);
+    render(){
+        return (<div className="Gallery">
+            <div className="lander">
+            <div id="instafeed"></div>
+            </div>
+            </div>);
     }
 }
